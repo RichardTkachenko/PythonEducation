7af2a62956bd769706300ced8628e9502d454eb3
Richard Tkachenko <richard.n.tkachenko@gmail.com>
2024-08-25 22:45:40 +0300

    first commit

---
+++ .venv/pyperclip.py
@@ -0,0 +1,6 @@
+import pyperclip
+  pyperclip.copy('The text to be copied to the clipboard.')
+  spam = pyperclip.paste()
+
+  if not pyperclip.is_available():
+    print("Copy functionality unavailable!")
